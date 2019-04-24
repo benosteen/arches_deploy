d60a6dff1f9681bfff16b56386863d67539d6acc
benosteen <bosteen@gmail.com>
2019-04-24 14:30:19 -0700

    Adding basic settings templating for the LDAP configuration

--- settings_local.py
+++ settings_local.py
@@ -15,15 +15,15 @@ def get_env_variable(var_name):
         raise ImproperlyConfigured(error_msg)
 
 
-def get_optional_env_variable(var_name):
+def opt_env(var_name, default_response = ""):
     try:
         return os.environ[var_name]
     except KeyError:
-        return None
+        return default_response
 
 ARCHES_NAMESPACE_FOR_DATA_EXPORT = 'http://localhost:8000/'
 
-USER_ARCHES_NAMESPACE_FOR_DATA_EXPORT = get_optional_env_variable('ARCHES_NAMESPACE_FOR_DATA_EXPORT')
+USER_ARCHES_NAMESPACE_FOR_DATA_EXPORT = opt_env('ARCHES_NAMESPACE_FOR_DATA_EXPORT')
 if USER_ARCHES_NAMESPACE_FOR_DATA_EXPORT:
     # Make this unique, and don't share it with anybody.
     ARCHES_NAMESPACE_FOR_DATA_EXPORT = USER_ARCHES_NAMESPACE_FOR_DATA_EXPORT
@@ -75,13 +75,13 @@ ELASTICSEARCH_HOSTS = [
     #{'host': get_env_variable('ESHOST'), 'port': 443, "use_ssl": True}
 ]
 
-USER_ELASTICSEARCH_PREFIX = get_optional_env_variable('ELASTICSEARCH_PREFIX')
+USER_ELASTICSEARCH_PREFIX = opt_env('ELASTICSEARCH_PREFIX')
 if USER_ELASTICSEARCH_PREFIX:
     ELASTICSEARCH_PREFIX = USER_ELASTICSEARCH_PREFIX
 
 ALLOWED_HOSTS = get_env_variable('DOMAIN_NAMES').split()
 
-USER_SECRET_KEY = get_optional_env_variable('DJANGO_SECRET_KEY')
+USER_SECRET_KEY = opt_env('DJANGO_SECRET_KEY')
 if USER_SECRET_KEY:
     # Make this unique, and don't share it with anybody.
     SECRET_KEY = USER_SECRET_KEY
@@ -93,3 +93,51 @@ APP_NAME = 'AATA'
 APP_TITLE = 'AATA | Arches'
 COPYRIGHT_TEXT = ''
 COPYRIGHT_YEAR = '2019'
+
+
+#############################################
+# ==============LDAP CONFIG START============
+#############################################
+
+# set the following to enable the LDAP authentication backend
+# To install:
+# sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev
+# (ENV) $ pip install django-auth-ldap
+
+ARCHES_USE_LDAP = opt_env("USE_LDAP", False)
+
+if ARCHES_USE_LDAP:
+    import ldap
+    from django_auth_ldap.config import LDAPSearch
+
+    # Baseline configuration.
+    AUTHENTICATION_BACKENDS = ('django_auth_ldap.backend.LDAPBackend',) + AUTHENTICATION_BACKENDS
+    AUTH_LDAP_SERVER_URI = get_env_variable("AUTH_LDAP_SERVER_URI")
+
+
+    # Some ldap options for TLS (https://python-ldap.readthedocs.io/en/latest/reference/ldap.html#ldap-options)
+    # AUTH_LDAP_CONNECTION_OPTIONS = {
+    #   ldap.OPT_REFERRALS: 0,
+    #   ldap.OPT_PROTOCOL_VERSION: 3
+    #   ldap.OPT_X_TLS_CACERTFILE: '/absolute/path/to/ldap/cert/file.pem',
+    #   ldap.OPT_X_TLS: ldap.OPT_X_TLS_DEMAND,
+    #   ldap.OPT_X_TLS_DEMAND: True,
+    # }
+
+    # By default, all mapped user fields will be updated each time the user logs in.
+    # To disable this, set AUTH_LDAP_ALWAYS_UPDATE_USER to False
+    AUTH_LDAP_ALWAYS_UPDATE_USER = True   # Default
+
+    AUTH_LDAP_BIND_DN = opt_env("AUTH_LDAP_BIND_DN")
+    AUTH_LDAP_BIND_PASSWORD = opt_env("AUTH_LDAP_BIND_PASSWORD")
+    AUTH_LDAP_USER_SEARCH = LDAPSearch(
+        get_env_variable("AUTH_LDAP_BASE_DN_SEARCH"),
+        ldap.SCOPE_SUBTREE,
+        '(uid=%(user)s)',
+    )
+    # Or:
+    #AUTH_LDAP_USER_DN_TEMPLATE = 'uid=%(user)s,ou=users,dc=example,dc=org'
+
+    AUTH_LDAP_USER_ATTR_MAP = {"first_name": "givenName",
+                               "last_name": "sn",
+                               "email": "mail"}
